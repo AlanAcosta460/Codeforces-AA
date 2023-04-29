5b8b8a4c4667341924cb0c2ee1490739db6ca7e7
Alan Acosta <alanomar460@gmail.com>
2023-04-29 16:55:47 -0600

    546A

--- 800/soldierAndBananas.cpp
+++ 800/soldierAndBananas.cpp
@@ -4,7 +4,7 @@ int main() {
     int k, w, count=0;
     long int n;
     std::cin>>k>>n>>w;
-    for(int i=1; i<=w; i++) {count += i*k;}
+    for(int i=1; i<=w; i++) count += i*k;
     if (count > n) std::cout<<count-n;
     else std::cout << '0';
     return 0;
