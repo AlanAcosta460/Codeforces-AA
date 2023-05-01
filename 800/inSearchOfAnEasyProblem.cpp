86c66ff3431b9ecaabea61b5df943bdf555c95e9
Alan Acosta <alanomar460@gmail.com>
2023-04-30 20:12:06 -0600

    1030A

---
+++ 800/inSearchOfAnEasyProblem.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n, imput;
+    std::cin>>n;
+    for (int i = 0; i < n; i ++) {std::cin>>imput; if(imput==1) {std::cout<<"HARD"; exit(0);}}
+    std::cout<<"EASY";
+    return 0;
+}
