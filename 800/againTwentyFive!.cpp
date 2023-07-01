ff3896295ba805647be1122c42c1c2d8474341e5
Alan Acosta <alanomar460@gmail.com>
2023-07-01 10:03:44 -0600

    630A

---
+++ 800/againTwentyFive!.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+void solve() {
+    int n;
+    std::cin>>n;
+    std::cout<<"25";
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    int tc = 1;
+    // std::cin>>tc;
+    for (int t = 1; t <= tc; t++) solve();
+}
